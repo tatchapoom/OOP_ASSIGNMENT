27c89d53624a48f97586c8a00d28b1dfc82716ef
TAN <tatchapoom@hotmail.com>
2016-04-15 23:57:19 +0700

    2.สร้างคลาส GamePanel เพื่อเป็นหน้าต่างที่จะใช้แสดงเกมส์

--- f2/spw/GamePanel.java
+++ f2/spw/GamePanel.java
@@ -5,9 +5,26 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
-
+import java.awt.Toolkit;
+import java.awt.Image;
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel {
-
+	private BufferedImage bi;
+	Graphics2D big;
+ 	public GamePanel() {
+  		bi = new BufferedImage(400, 600, BufferedImage.TYPE_INT_ARGB);
+  		big = (Graphics2D) bi.getGraphics();
+  		big.setBackground(Color.BLACK);	
+ 	}
+ 	public void updateGameUI(GameReporter reporter){
+  		big.clearRect(0, 0, 400, 600);
+  		big.setColor(Color.RED);	
+  		repaint();	
+ 	} 	
+ 	@Override
+ 	public void paint(Graphics g) {
+ 		Graphics2D g2d = (Graphics2D) g;
+ 		g2d.drawImage(bi, null, 0, 0);
+ 	} 	
 }
--- f2/spw/Main.java
+++ f2/spw/Main.java
@@ -1,16 +1,23 @@
 package f2.spw;
+import javax.imageio.ImageIO;
+import java.io.IOException;
+import java.io.File;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-import java.awt.BorderLayout;
-
-import javax.swing.JFrame;
 
 public class Main {
 	public static void main(String[] args){
+		JButton b1;
+		JLabel l1;
 		JFrame frame = new JFrame("Space War");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(400, 650);
 		frame.getContentPane().setLayout(new BorderLayout());
-		frame.setVisible(true);
+ 		GamePanel gp = new GamePanel();
+ 		frame.getContentPane().add(gp, BorderLayout.CENTER);
+		frame.setVisible(true);    	
 
 	}
 }
